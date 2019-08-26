325ee687395dbc44512dce4fb0bcf8dd7eab2190
weihua <weihua@springboot>
2019-08-26 16:16:57 +0800

    2019-8-26

---
+++ src/main/java/com/admin/springboot/SpringBootAdminClientApplication.java
@@ -0,0 +1,14 @@
+package com.admin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientApplication.class, args);
+	}
+
+}
